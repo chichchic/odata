051465421d1328a81ef5e9961ba440d8d5181a90
chicichich <pch6789@naver.com>
2021-10-12 15:35:55 +0900

    feat: change path of (example)index.js file

---
+++ example/index.js
@@ -0,0 +1,13 @@
+import requestingResource from './Requesting_resource.js'
+import requestingIndividual from './Requesting_individual.js'
+import queries from './Queries.js'
+import creatingResource from './Creating_resource.js'
+import relatingResource from './Relating_resource.js'
+import invokingFunction from './Invoking_function.js'
+
+// requestingResource();
+// requestingIndividual();
+// queries();
+// creatingResource();
+// relatingResource();
+invokingFunction();
