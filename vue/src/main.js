361fa227ab2808252782c69c27dd504c3322557b
chicichich <pch6789@naver.com>
2021-10-18 08:22:34 +0900

    feat: add elemnt-ui table, table-column

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,6 +1,12 @@
 import { createApp } from "vue";
+import { ElTable, ElTableColumn } from "element-plus";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(ElTable)
+  .use(ElTableColumn)
+  .mount("#app");
